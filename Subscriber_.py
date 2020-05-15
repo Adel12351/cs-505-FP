f20115062dbc532c401271245320fcf264de2e90
adel12351 <vip.sa.2020@hotmail.com>
2020-05-15 06:25:19 +0300

    subscriber file is done

---
+++ Subscriber_.py
@@ -0,0 +1,41 @@
+import pika
+import sys
+import json
+
+username = 'student'
+password = 'student01'
+hostname = '128.163.202.61'
+virtualhost = 'patient_feed'
+#5672  
+
+credentials = pika.PlainCredentials(username, password)
+parameters = pika.ConnectionParameters(hostname,5672,virtualhost,credentials)
+
+connection = pika.BlockingConnection(parameters)
+channel = connection.channel()
+
+exchange_name = 'patient_data'
+channel.exchange_declare(exchange=exchange_name, exchange_type='topic')
+
+result = channel.queue_declare('', exclusive=True)
+queue_name = result.method.queue
+
+binding_keys = "#"
+
+if not binding_keys:
+    sys.stderr.write("Usage: %s [binding_key]...\n" % sys.argv[0])
+    sys.exit(1)
+
+for binding_key in binding_keys:
+    channel.queue_bind(exchange=exchange_name, queue=queue_name, routing_key=binding_key)
+
+print(' [*] Waiting for logs. To exit pres CTRL+C')
+
+def callback(ch, method, properties, body):
+    print(" [x] %r:%r" % (method.routing_key, body))
+
+
+channel.basic_consume(
+    queue=queue_name, on_message_callback=callback,auto_ack=True)
+
+channel.start_consuming()
